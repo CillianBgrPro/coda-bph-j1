7b5013bc9df6e16fbb251b97707f838bd85db9fe
Cillian <baugercillianpro@gmail.com>
2025-11-20 11:29:15 +0100

    First Exercice

---
+++ types-et-variables/exercice_1.php
@@ -0,0 +1,36 @@
+<?php
+
+$nb = 42;
+echo $nb;
+
+echo "<br>";
+
+$nb_tab = [1, 2, 3, 3.5, 4];
+print_r($nb_tab);
+
+echo "<br>";
+
+$species = "chat";
+$age = "10 ans";
+$name = "Curly";
+
+$animal = [$species, $name, $age];
+
+echo "Un " . $species . " de " . $age . " appelé " . $name;
+
+echo "<br>";
+
+$data = 42.0;
+echo $data;
+
+echo "<br>";
+
+$int = (int) 42.7;
+echo $int;
+
+echo "<br>";
+
+$str = (int) "42";
+echo $str;
+
+?>
